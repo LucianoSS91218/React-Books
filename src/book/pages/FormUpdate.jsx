effebd52d26b0e27133a4fe9dc2f1df5f87d4fad
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-04 17:20:02 -0300

    Create FormUpdate.jsx

---
+++ src/book/pages/FormUpdate.jsx
@@ -0,0 +1,200 @@
+import validate from "../components/form/validateInfo";
+
+import { useState, useEffect } from "react";
+import { useBooks } from "../store zustand/Store";
+
+const inputStyles = {
+  formContainer: {
+    width: "400px",
+    margin: "0 auto",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "5px",
+    margin: "15px 0",
+  },
+  title: {
+    fontSize: "16px",
+    textAlign: "left",
+  },
+  input: {
+    padding: "10px",
+    borderRadius: "5px",
+    fontSize: "16px",
+  },
+  elerror: {
+    color: "red",
+    margin: 0,
+    padding: 0,
+  },
+  submit: {
+    padding: "15px 20px",
+    minWidth: "200px",
+    border: "none",
+    borderRadius: "5px",
+    backgroundColor: "#1e9638",
+    color: "white",
+    fontWeigth: "bolder",
+    fontSize: "18px",
+    cursor: "pointer",
+  },
+};
+
+const FormUpdate = ({
+  id,
+  title,
+  author,
+  cover,
+  completed,
+  intro,
+  review,
+  isEditing,
+  setIsEditing,
+}) => {
+  const [values, setValues] = useState({
+    title: title,
+    author: author,
+    cover: cover,
+    completed: completed,
+    intro: intro,
+    review: review,
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value, checked } = e.target;
+    setValues({
+      ...values,
+      [name]: name === "completed" ? checked : value,
+    });
+
+    if (e.target.checked) {
+      values.completed = true;
+    }
+    setErrors(validate(values));
+  };
+
+  function handleOnChangeFile(e) {
+    const element = e.target;
+    var file = element.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+
+      values.cover = reader.result.toString();
+    };
+    reader.readAsDataURL(file);
+  }
+
+  const { updateItem } = useBooks();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      values.title === "" ||
+      values.author === "" ||
+      values.intro === "" ||
+      values.review === ""
+    ) {
+      return;
+    }
+
+    updateItem({
+      id: id,
+      title: values.title,
+      author: values.author,
+      completed: values.completed,
+      cover: values.cover,
+      intro: values.intro,
+      review: values.review,
+    });
+
+    setIsEditing("");
+  };
+
+  useEffect(() => {}, [isEditing]);
+
+  return (
+    <form onSubmit={handleSubmit} style={inputStyles.formContainer}>
+      <div style={inputStyles.container}>
+        <div style={inputStyles.title}>Title</div>
+        <input
+          style={inputStyles.input}
+          type="text"
+          name="title"
+          onChange={handleChange}
+          value={values.title}
+        />
+        {errors.title && <p style={inputStyles.elerror}>{errors.title}</p>}
+      </div>
+      <div style={inputStyles.container}>
+        <div style={inputStyles.title}>Author</div>
+        <input
+          style={inputStyles.input}
+          type="text"
+          name="author"
+          onChange={handleChange}
+          value={values.author}
+        />
+        {errors.author && <p style={inputStyles.elerror}>{errors.author}</p>}
+      </div>
+
+      <div style={inputStyles.container}>
+        <div style={inputStyles.title}>Completed</div>
+        <input
+          style={inputStyles.input}
+          type="checkbox"
+          name="completed"
+          onChange={handleChange}
+          value={values.completed}
+          checked={values.completed}
+        />
+      </div>
+
+      <div style={inputStyles.container}>
+        <div style={inputStyles.title}>Cover</div>
+        <input type="file" name="cover" onChange={handleOnChangeFile} />
+        <div>
+          {!!values.cover ? <img src={values.cover} width="200" /> : ""}
+        </div>
+      </div>
+
+      <div style={inputStyles.container}>
+        <div style={inputStyles.title}>intro</div>
+        <input
+          style={inputStyles.input}
+          type="text"
+          name="intro"
+          onChange={handleChange}
+          value={values.intro}
+        />
+        {errors.intro && <p style={inputStyles.elerror}>{errors.intro}</p>}
+      </div>
+
+      <div style={inputStyles.container}>
+        <div style={inputStyles.title}>review</div>
+        <input
+          style={inputStyles.input}
+          type="text"
+          name="review"
+          onChange={handleChange}
+          value={values.review}
+        />
+        {errors.review && <p style={inputStyles.elerror}>{errors.review}</p>}
+      </div>
+
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        value="Update book"
+        style={inputStyles.submit}
+      >
+        Update
+      </button>
+    </form>
+  );
+};
+
+export default FormUpdate;

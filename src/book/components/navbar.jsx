983db65d91950a73cb898c678158368121557695
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-04 18:14:47 -0300

    Create navbar.jsx

---
+++ src/book/components/navbar.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  const linkStyle = {
+    padding: "10px",
+    fontSize: "22px",
+    textDecoration: "none",
+    color: "#090909",
+  };
+
+  const linkStyleTwo = {
+    padding: "10px",
+    fontSize: "23px",
+    textDecoration: "none",
+    color: "#fcfcfc",
+    fontWeight: "800",
+  };
+
+  const navContainerStyle = {
+    backgroundColor: "#038285",
+    padding: "10px",
+    display: "flex",
+    justifyContent: "space-around",
+  };
+
+  return (
+    <div style={navContainerStyle}>
+      <Link to="/" style={linkStyle}>
+        HOME
+      </Link>
+      <Link to="/create" style={linkStyleTwo}>
+        Create
+      </Link>
+    </div>
+  );
+}
